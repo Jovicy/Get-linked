c4f3f3abaa2adf73918f6bc40579d737a235ae41
unknown <johnvictordml@gmail.com>
2023-09-20 19:54:35 +0100

    redeploying

--- src/shared/ActionButton.tsx
+++ src/shared/ActionButton.tsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import { SelectedPage } from './types';
+// import React from 'react'
+// import AnchorLink from 'react-anchor-link-smooth-scroll'
 
-type Props = {
-    children: React.ReactNode;
-    setSelectedPage: (value: SelectedPage) => void;
-}
+// type Props = {
+//     children: React.ReactNode;
+//     setSelectedPage: (value: SelectedPage) => void;
+// }
 
-const ActionButton = ({ children, setSelectedPage }: Props) => {
-  return (
-    <AnchorLink
-    className='bg-gradient-to-r from-pink-500 to-purple-500 text-white w-40 h-12 rounded font-montserrat font-normal text-base'
-    onClick={() => setSelectedPage(SelectedPage.ContactUs)}
-    href={`#${SelectedPage.ContactUs}`}
-    >
-        {children}
-    </AnchorLink>
-  )
-}
+// const ActionButton = ({ children, setSelectedPage }: Props) => {
+//   return (
+//     <AnchorLink
+//     className='bg-gradient-to-r from-pink-500 to-purple-500 text-white w-40 h-12 rounded font-montserrat font-normal text-base'
+//     onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+//     href={`#${SelectedPage.ContactUs}`}
+//     >
+//         {children}
+//     </AnchorLink>
+//   )
+// }
 
-export default ActionButton
+// export default ActionButton
